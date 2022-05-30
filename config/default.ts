89f3c2b255ca94d127ea5fe03cf0c2a72e1e594b
Szabó Gábor <gabor_szabo1@epam.com>
2022-05-30 21:15:59 +0200

    node-app

---
+++ config/default.ts
@@ -0,0 +1,12 @@
+export default {
+  port: 1337,
+  dbUri:
+    "mongodb+srv://root:root@cluster0.rr834.mongodb.net/?retryWrites=true&w=majority",
+  saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
+  accessTokenPrivateKey: ``,
+  accessTokenPublicKey: ``,
+  refreshTokenPrivateKey: ``,
+  refreshTokenPublicKey: ``,
+};
